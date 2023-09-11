d101bd858700ea9882fe2043ad19f7c6163cd296
Lucas Alves <mendeslucas277@gmail.com>
2023-09-10 23:33:06 -0300

    style: ajustando layout dos icones de navegação

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,16 +8,41 @@ import { User } from 'src/app/Interfaces/user';
 })
 export class HeaderComponent {
 
-  date = new Date();
+  private date = new Date();
+
   formatedDate = `${this.date.getFullYear()}-${String(this.date.getMonth() + 1).padStart(2, '0')}-${String(this.date.getDate()).padStart(2, '0')} ${String(this.date.getHours()).padStart(2, '0')}:${String(this.date.getMinutes()).padStart(2, '0')}:${String(this.date.getSeconds()).padStart(2, '0')}`;
   public user: User = {
-    name: '',
+    name: 'Lucas',
+    surname: 'Mendes',
     email: '',
     password: '',
     image: '../../assets/icons/user-icon.svg',
     age: 0,
     created_at: this.formatedDate
   };
+
+  public links: Array<{ name: string, link: string, img: string }> = [
+    {
+      name: 'Home',
+      link: '/home',
+      img: '../../../assets/icons/home-icon.svg'
+    },
+    {
+      name: 'About',
+      link: '/about',
+      img: '../../../assets/icons/',
+    },
+    {
+      name: 'Contact',
+      link: '/contact',
+      img: '../../../assets/icons/',
+    },
+    {
+      name: 'Login',
+      link: '/login',
+      img: '../../../assets/icons/',
+    }
+  ];
 
   constructor() {
   }
