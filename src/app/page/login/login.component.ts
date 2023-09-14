4795d9e1f32115574bd17e085b9c8504aaf04e4e
Lucas Alves <mendeslucas277@gmail.com>
2023-09-13 22:19:30 -0300

    fix: ajustando tela de login

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public form = FormGroup;
 
+  formLogin = this.fb.group({
+    email: ['', [Validators.min(4), Validators.email, Validators.required]],
+    password: ['', [Validators.min(4), Validators.required]],
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.formLogin.reset();
+  }
 }
